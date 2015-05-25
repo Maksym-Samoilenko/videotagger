b27e7705ee4fb881951ac6232f4838976f6f769a
hp <maxsam125@gmail.com>
2015-05-26 00:51:19 +0300

    Changed default port.

--- server.js
+++ server.js
@@ -43,4 +43,6 @@ app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(9001);
+app.listen(process.env.PORT || 3000, function(){
+  console.log('listening on', process.env.PORT || 3000);
+});
